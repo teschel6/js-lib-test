0e9c70eaf3b54f05d1ecc343bad381eb3ba78841
teschel6 <elijah.tesch@gmail.com>
2024-10-19 11:10:44 -0600

    push failing test

--- test/index.test.ts
+++ test/index.test.ts
@@ -2,6 +2,6 @@ import { add } from '../src/index';
 
 describe('testing index file', () => {
   test('empty string should result in zero', () => {
-    expect(add('')).toBe(0);
+    expect(add('')).toBe(1);
   });
 });
